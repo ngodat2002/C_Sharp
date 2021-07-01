47db613e953434d479021f6636ecc16c61fc7da1
ngodat2002 <73330656+ngodat2002@users.noreply.github.com>
2021-07-01 15:16:35 +0700

    demo

---
+++ Task06/Session7/Codesnipet6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Codesnipet6
+{
+    class Animal
+    {
+        public Animal()
+        {
+            Console.WriteLine("Animal contrucstor without parametters");
+        }
+        public Animal(String name)
+        {
+            Console.WriteLine("Animal constructor with a string parameters");
+
+        }
+    }
+    class Canie : Animal
+    {
+        public Canie() : base("Lion")
+        {
+            Console.WriteLine("Derived Canine");
+        }
+        static void Main(string[] args)
+        {
+            Canie objCanie = new Canie();
+        }
+    }
+}
